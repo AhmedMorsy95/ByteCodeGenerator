3e88b98b3fb74df509c3cff3eefdc67bd0985fea
Louay Morsi <louay.hesham@gmail.com>
2019-05-20 02:43:36 +0200

    Code cleaning

--- Phase3/main.c
+++ Phase3/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include "parse.tab.h"
 
-int main(){
-
-		yyparse();
-
-
-return 0;
+int main(int argc, char **argv)
+{
+	if ((argc > 1) && (freopen(argv[1], "r", stdin) == NULL))
+  {
+		printf("%s: File %s cannot be opened", argv[0], argv[1]);
+    return 1;
+  }
+	yyparse();
+	return 0;
 }
